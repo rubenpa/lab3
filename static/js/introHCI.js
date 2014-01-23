7378e9d98fa480b80f3b477256d1a8d7ba4d527b
Ruben Pierre-Antoine <rubenpa@stanford.edu>
2014-01-22 16:21:08 -0800

    my changes

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -12,7 +12,22 @@ function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
+	projectClick();
+	$('#submitBtn').click(updateProject);
+}
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+function projectClick() {	
+	$('.project').click(function(e) {
+		$(this).children().children('p, img').toggleClass('hidden');
+	});
+}
+
+function updateProject (e) {
+	var projectID = $('#project').val();
+	$(projectID).animate({
+		width: $('#width').val()
+	});
+
+	var newText = $('#description').val();
+	$(projectID).children().children('p').text(newText);
 }
